bceee4ee51c81da2dcf2ea95be00f63f34b6cb99
Daniel Kanczuk <dekanczuk@stefanini.com>
2016-11-25 13:19:07 -0200

    Tela de cadastro do aluno

--- program.c
+++ program.c
@@ -26,6 +26,42 @@ void main(){
 		printf("4 - Relatório de desempenho\n");
 		printf("Digite um valor:");
 		scanf("%d",&opt);
+		
+		//Cadastrar novo aluno
+		if(opt == 1){
+			system("cls");
+			//Verifica em qual registro deveremos escrever na memória
+			int i = 0;
+			while(aluno[i].ra != 0){
+				i = i + 1;
+			}
+			
+			//Valores temporários
+			int inputRA = 0;
+			char inputNome[50];
+			
+			//Escreve NOME aluno
+			printf("Digite o nome do aluno:");
+			scanf("%s",&inputNome);
+			
+			//Escreve RA aluno
+			printf("Digite o RA do aluno:");
+			scanf("%d",&inputRA);
+			
+			if(checkIfExistRA(inputRA) == -1 && checkNullRA(inputRA) == 1){
+				//Se existir já um RA ou for nulo
+				printf("\nRA invalido ou já existente!\n");
+				system("pause");
+				system("cls");
+			}else{
+				//Registra os valores
+				strcpy(aluno[i].nome,inputNome);
+				aluno[i].ra = inputRA;
+				printf("\nCadastro realizado com sucesso!\n");
+				system("pause");
+				system("cls");
+			}
+		}
 	}
 		
 		
